88a1626cae68ca5f95e029ca889e42f2d423623e
JesusGalea <93735071+JesusGalea@users.noreply.github.com>
2022-01-31 23:23:55 +0100

    arreglo

--- ifarray/src/ifarray/If4.java
+++ ifarray/src/ifarray/If4.java
@@ -17,13 +17,13 @@ public class If4 {
 
     String con1,con2,con3,con4;
 
-    con1 = (a>0)?"positivo": "negativo";
+    con1 = (a>0)?"positivo, ": "negativo, ";
 
-    con2 = (a%2==0)?"par": "impar";
+    con2 = (a%2==0)?"par, ": "impar, ";
 
-    con3 = (a%5== 0)?"Multiplo de 5": "No multiplo de 5";
+    con3 = (a%5== 0)?"Multiplo de 5, ": "No multiplo de 5, ";
 
-    con4 = (a<100)?"mayor a 100": "menor a 100";
+    con4 = (a<100)?"menor a 100, ": "mayor a 100, ";
 
 
 
